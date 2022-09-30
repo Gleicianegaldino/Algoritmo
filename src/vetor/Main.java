6d05ac5c7be56017f650d53405e944128d122c0b
Gleiciane Galdino <gleicianegaldino25@gmail.com>
2022-09-30 20:46:13 -0300

    Código simples

---
+++ src/vetor/Main.java
@@ -0,0 +1,15 @@
+package vetor;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		int x = 3;
+		System.out.println("Valor de X: " + x);
+		
+		x = 4;
+		
+		System.out.println("Novo valor de X: " + x);
+		
+	}
+	
+}
