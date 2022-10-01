c61ddf1e1bf2edc12819efe29454213349ca01d6
Gleiciane Galdino <gleicianegaldino25@gmail.com>
2022-09-30 21:42:35 -0300

    Busca linear

--- src/aula02_vetor/Main.java
+++ src/aula02_vetor/Main.java
@@ -1,5 +1,7 @@
 package aula02_vetor;
 
+import java.util.Scanner;
+
 public class Main {
 	
 	public static void main(String[] args) {
@@ -19,6 +21,27 @@ public class Main {
 			System.out.print("Estados " + i + ": " + estados[i] + "\n");
 		}
 		
+		Scanner leitor = new Scanner(System.in);
+		System.out.println("DIGITE A SIGLA DO ESTADO PARA A BUSCA");
+		String siglaBusca = leitor.nextLine();
 		
+		//BUSCA LINEAR
+		
+		boolean encontrou = false;
+		
+		for(int i = 0; i < estados.length; i++) {
+			String elemento = estados[i];
+			
+			if(elemento.equalsIgnoreCase(siglaBusca)) {
+				encontrou = true;
+				break;
+			}
+		}
+		
+		if (encontrou == true) {
+			System.out.println("ENCONTROU");
+		}else {
+			System.out.println("NÃO ENCONTROU");
+		}
 	}
 }
