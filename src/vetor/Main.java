c28070db7a03ccd4bc632581c90cfba79095ad1b
Gleiciane Galdino <gleicianegaldino25@gmail.com>
2022-09-30 21:04:38 -0300

    Vetor numérico

--- src/vetor/Main.java
+++ src/vetor/Main.java
@@ -3,13 +3,21 @@ package vetor;
 public class Main {
 	public static void main(String[] args) {
 		
-		int x = 3;
-		System.out.println("Valor de X: " + x);
+		int[] vetorzao = new int[10];
+		vetorzao[0] = 3;
+		vetorzao[1] = 4;
+		vetorzao[2] = 5;
+		vetorzao[3] = 33;
+		vetorzao[4] = 37;
+		vetorzao[5] = 2;
+		vetorzao[6] = 7;
+		vetorzao[7] = 1;
+		vetorzao[8] = 302;
+		vetorzao[9] = 44;
 		
-		x = 4;
-		
-		System.out.println("Novo valor de X: " + x);
-		
+		for (int i=0; i < vetorzao.length; i++) {
+			System.out.println(vetorzao[i]);
+			
+		}
 	}
-	
 }
